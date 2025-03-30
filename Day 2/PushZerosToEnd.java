5f96dede46cd5feb4a869ed873e1143bd114e9c7
ronny205 <rohanaiwale99@gmail.com>
2025-03-30 10:23:41 +0530

    feat: Solved "Move All Zeroes to End" problem (Day 2/160)

---
+++ Day 2/PushZerosToEnd.java
@@ -0,0 +1,19 @@
+class PushZerosToEnd {
+    void pushZerosToEnd(int[] arr) {
+        
+        int n = arr.length;
+        int count = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0){
+                arr[count] = arr[i];
+                count++;
+            }
+        }
+        
+        while ( count < n){
+            arr[count] = 0;
+            count++;
+        }
+    }
+}
