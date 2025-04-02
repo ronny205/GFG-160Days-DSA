5bfa70e14ab40013fb5193259bd723b2615b56d5
ronny205 <rohanaiwale99@gmail.com>
2025-04-02 14:12:11 +0530

    Solved "Next Permutation" problem (Day 5/160)

---
+++ Day 5/NextPermutation.java
@@ -0,0 +1,28 @@
+class nextPermutation {
+    void nextPermutation(int[] arr) {
+        int i = arr.length - 2;
+        
+        while ( i >= 0 && arr[i] >= arr[i + 1]) i--;
+        
+        if(i >= 0){
+            int j = arr.length - 1;
+            while ( arr[j] <= arr[i]) j--;
+            swap(arr, i, j);
+        }
+        
+        reverse(arr, i + 1);
+    }
+    
+    void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    void reverse(int[] arr, int start){
+        int end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+}
