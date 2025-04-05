b1151733509c61efef4299794ec271c954178908
ronny205 <rohanaiwale99@gmail.com>
2025-04-05 19:31:08 +0530

    Solved "Buy and Sell Stock - Max one Transaction Allowed" problem

---
+++ Day 8/StockBuySellWIthLimit.java
@@ -0,0 +1,20 @@
+class StockBuySellWithLimit{
+
+    public int maximumProfit(int prices[]) {
+        if (prices == null || prices.length < 2) return 0;
+        
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int profit = prices[i] - minPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+        }
+        
+        return maxProfit;
+    }
+}
